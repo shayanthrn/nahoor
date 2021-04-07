e005737f697f0352a5d90ec88c26e86deb9e3f09
Shayan Tehrani <shayanthrn@gmail.com>
2021-04-07 09:41:26 +0430

    start tempelate

--- public/assets/js/config.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("base").setAttribute("href", "https://reservation.drtajviz.com");
-// document.getElementById("base").setAttribute("href","http://localhost");
--- public/assets/panelassests/hiddenForm.js
+++
@@ -1,5 +0,0 @@
-$hiddenForm = $("form.hidden-form")
-
-$(".hidden-form .edit-button, .hidden-form .back-button").on('click', function () {
-  $hiddenForm.toggleClass("edit")
-})
